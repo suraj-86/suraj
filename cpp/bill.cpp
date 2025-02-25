7ac61827fb5bb94d824990af9ecbbee9a8d7ca19
suraj-86 <harshsuraj8676@gmail.com>
2025-02-25 08:49:34 +0530

    Added new files

--- cpp/bill.cpp
+++ cpp/bill.cpp
@@ -5,10 +5,10 @@ the program must calculate and display the final bill*/
 #include<iostream>
 using namespace std;
 
-
 class product{
     public:
         int code;
+        string name;
         float price;
         int quantity;
     
@@ -16,27 +16,29 @@ class product{
             cout<< "Details for item \n";
             cout << "Enter code: ";
             cin >> code;
+            cout<<"Enter Name: ";
+            cin>>name;
             cout << "Enter price: ";
             cin >> price;
             cout << "Enter quantity: ";
             cin >> quantity;
         }
-    
         void bill_print() {                    
-            cout<< "Final Bill:\n\n";
-            cout<< "Code  Price  Quantity\n";
-            cout << code <<"   " << price << "    " << quantity << "\n";
+            cout<< "Code  Name  Price  Quantity\n";
+            cout << code <<"   " <<name <<"    "<< price << "    " << quantity << "\n";
         }
 };
-    
-    
 int main(){
     product item;
-
     item.input();
 
+    cout<<"\n\nSURAJ'S SUPER MARKET\n";
+    cout<<"-----------------------------";
+    cout<< "\nFinal Bill:\n";
+    cout<<"-----------------------------\n";
     float grandTotal = 0;
     item.bill_print();
     grandTotal += item.price * item.quantity;
-    cout << "\nGrand Total: " << grandTotal << "\n";
+    cout<<"-----------------------------\n";
+    cout << "Grand Total: " << grandTotal << "\n";
 }
---
+++ cpp/billx.cpp
@@ -0,0 +1,53 @@
+//bill for several products
+
+
+#include<iostream>
+using namespace std;
+
+class product{
+    public:
+        int code;
+        string name;
+        float price;
+        int quantity;
+        int total;
+    
+        void input() {
+            cout << "Enter code: ";
+            cin >> code;
+            cout <<"Enter name: ";
+            cin >>name;
+            cout << "Enter price: ";
+            cin >> price;
+            cout << "Enter auantity: ";
+            cin >> quantity;
+        }
+        void bill_print() {            
+            total=price*quantity;        
+            cout << code <<"   " << name <<"   " << price << "    " << quantity << "    " << total << "\n";
+        }
+};
+    
+int main(){
+    int a,i;
+    cout<<"no of products bought : ";
+    cin>>a;
+
+    product item[100];
+    float grandTotal = 0;
+    for(i=1;i<=a;i++){
+        cout<<"\nEnter details of Product "<< i <<"\n";
+        item[i].input();
+    }
+    cout<<"\n\nSURAJ'S SUPER MARKET\n";
+    cout<<"-----------------------------";
+    cout<< "\nFinal Bill:\n";
+    cout<<"-----------------------------\n\n";
+    cout<< "Code  Name  Price  Quantity Total\n";
+    for(i=1;i<=a;i++){
+        item[i].bill_print();
+        grandTotal += item[i].total;
+    }
+    cout<<"\n-----------------------------\n";
+    cout << "Grand Total : " << grandTotal << "\n";
+}
